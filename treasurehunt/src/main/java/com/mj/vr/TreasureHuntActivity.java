fec23ec42b9017e7ce6eb04d145929cb7451e7cf
hgl888 <hgl868@126.com>
2017-01-12 14:17:38 +0800

    添加程序代码

--- treasurehunt/src/main/java/com/mj/vr/TreasureHuntActivity.java
+++ treasurehunt/src/main/java/com/mj/vr/TreasureHuntActivity.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.google.vr.sdk.samples.treasurehunt;
+package com.mj.vr;
 
 import android.content.Context;
 import android.opengl.GLES20;
--- treasurehunt/src/main/java/com/mj/vr/WorldLayoutData.java
+++ treasurehunt/src/main/java/com/mj/vr/WorldLayoutData.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.google.vr.sdk.samples.treasurehunt;
+package com.mj.vr;
 
 /**
  * Contains vertex, normal and color data.
