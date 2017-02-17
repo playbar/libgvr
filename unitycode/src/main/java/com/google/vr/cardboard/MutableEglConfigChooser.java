bce4d72ca32c9a8cd525c354bd3ceb3ef531b0cb
hgl888 <hgl868@126.com>
2017-02-17 09:50:57 +0800

    修改程序代码

--- unitycode/src/main/java/com/google/vr/cardboard/MutableEglConfigChooser.java
+++ unitycode/src/main/java/com/google/vr/cardboard/MutableEglConfigChooser.java
@@ -13,7 +13,7 @@ import javax.microedition.khronos.egl.EGLDisplay;
 public class MutableEglConfigChooser implements EGLConfigChooser {
     private static final int EGL_MUTABLE_RENDER_BUFFER_BIT = 4096;
     private static final int EGL_OPENGL_ES3_BIT_KHR = 64;
-    private boolean forceMutableBuffer = true;
+    private boolean forceMutableBuffer = false;
 
     public MutableEglConfigChooser() {
     }
@@ -23,6 +23,17 @@ public class MutableEglConfigChooser implements EGLConfigChooser {
     }
 
     public EGLConfig chooseConfig(EGL10 var1, EGLDisplay var2) {
+
+        int [] config_num = new int[1];
+        var1.eglGetConfigs(var2, null, 0, config_num);
+
+        EGLConfig []configs = new EGLConfig[config_num[0]];
+        var1.eglGetConfigs(var2, configs, config_num[0], config_num);
+        for ( int i  = 0; i < config_num[0]; ++i){
+//            var1.eglGetConfigAttrib(var2, configs[i], )
+        }
+
+
 //        int[] var3 = new int[]{12324, 8, 12323, 8, 12322, 8, 12321, 0, 12325, 0, 12326, 0, 12352, 64, 12339, 4100, 12344};
         int[] attrlist = new int[]{
                 EGL10.EGL_RED_SIZE, 8,
@@ -33,6 +44,7 @@ public class MutableEglConfigChooser implements EGLConfigChooser {
                 EGL10.EGL_STENCIL_SIZE, 0,
                 EGL10.EGL_RENDERABLE_TYPE, 64,
                 EGL10.EGL_SURFACE_TYPE, EGL10.EGL_WINDOW_BIT,
+//                EGL10.EGL_MUTABLE_RENDER_BUFFER_BIT_KHR,
 //                EGL10.EGL_SURFACE_TYPE, 0x1004,
                 EGL10.EGL_NONE
         };
@@ -68,35 +80,35 @@ public class MutableEglConfigChooser implements EGLConfigChooser {
         EGLConfig[] var4 = var2;
         int var5 = var2.length;
 
-//        for(int var6 = 0; var6 < var5; ++var6) {
-//            EGLConfig var7 = var4[var6];
-//            int var8 = findConfigAttrib(var0, var1, var7, 12325, 0);
-//            int var9 = findConfigAttrib(var0, var1, var7, 12326, 0);
-//            int var10 = findConfigAttrib(var0, var1, var7, 12324, 0);
-//            int var11 = findConfigAttrib(var0, var1, var7, 12323, 0);
-//            int var12 = findConfigAttrib(var0, var1, var7, 12322, 0);
-//            int var13 = findConfigAttrib(var0, var1, var7, 12339, 0);
-//            if(var10 == 8 && var11 == 8 && var12 == 8 && var8 == 0 && var9 == 0 && (!var3 || (var13 & 4096) != 0)) {
-//                return var7;
-//            }
-//        }
-
-        for(int var6 = 0; var6 < var5; ++var6)
-        {
+        for(int var6 = 0; var6 < var5; ++var6) {
             EGLConfig var7 = var4[var6];
-            int var8 = findConfigAttrib(var0, var1, var7, EGL10.EGL_DEPTH_SIZE, 0);
-            int var9 = findConfigAttrib(var0, var1, var7, EGL10.EGL_STENCIL_SIZE, 0);
-            int var10 = findConfigAttrib(var0, var1, var7, EGL10.EGL_RED_SIZE, 0);
-            int var11 = findConfigAttrib(var0, var1, var7, EGL10.EGL_GREEN_SIZE, 0);
-            int var12 = findConfigAttrib(var0, var1, var7, EGL10.EGL_BLUE_SIZE, 0);
-            int var13 = findConfigAttrib(var0, var1, var7, EGL10.EGL_SURFACE_TYPE, 0);
-//            boolean bre = (var10 == 8 && var11 == 8 && var12 == 8 && var8 == 0 && var9 == 0 && (!var3 || (var13 & 4096) != 0));
-            boolean bre = (var10 == 8 && var11 == 8 && var12 == 8 && var8 == 0 && var9 == 0 );
-            if(bre)
-            {
+            int var8 = findConfigAttrib(var0, var1, var7, 12325, 0);
+            int var9 = findConfigAttrib(var0, var1, var7, 12326, 0);
+            int var10 = findConfigAttrib(var0, var1, var7, 12324, 0);
+            int var11 = findConfigAttrib(var0, var1, var7, 12323, 0);
+            int var12 = findConfigAttrib(var0, var1, var7, 12322, 0);
+            int var13 = findConfigAttrib(var0, var1, var7, 12339, 0);
+            if(var10 == 8 && var11 == 8 && var12 == 8 && var8 == 0 && var9 == 0 && (!var3 || (var13 & 4096) != 0)) {
                 return var7;
             }
         }
+
+//        for(int var6 = 0; var6 < var5; ++var6)
+//        {
+//            EGLConfig var7 = var4[var6];
+//            int var8 = findConfigAttrib(var0, var1, var7, EGL10.EGL_DEPTH_SIZE, 0);
+//            int var9 = findConfigAttrib(var0, var1, var7, EGL10.EGL_STENCIL_SIZE, 0);
+//            int var10 = findConfigAttrib(var0, var1, var7, EGL10.EGL_RED_SIZE, 0);
+//            int var11 = findConfigAttrib(var0, var1, var7, EGL10.EGL_GREEN_SIZE, 0);
+//            int var12 = findConfigAttrib(var0, var1, var7, EGL10.EGL_BLUE_SIZE, 0);
+//            int var13 = findConfigAttrib(var0, var1, var7, EGL10.EGL_SURFACE_TYPE, 0);
+////            boolean bre = (var10 == 8 && var11 == 8 && var12 == 8 && var8 == 0 && var9 == 0 && (!var3 || (var13 & 4096) != 0));
+//            boolean bre = (var10 == 8 && var11 == 8 && var12 == 8 && var8 == 0 && var9 == 0 );
+//            if(bre)
+//            {
+//                return var7;
+//            }
+//        }
 
         return null;
     }
