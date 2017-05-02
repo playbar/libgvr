37de888b660c6561d60e0a71bd23b484d342754d
playbar <hgl868@126.com>
2017-05-02 14:11:17 +0800

    修改代码

---
+++ unity/src/main/java/com/unity3d/player/ggc.java
@@ -0,0 +1,21 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.unity3d.player;
+
+import android.os.Build.VERSION;
+import com.unity3d.player.c;
+import com.unity3d.player.f;
+
+public final class ggc {
+    public static final boolean a;
+    public static final boolean b;
+    static final c c;
+
+    static {
+        a = VERSION.SDK_INT >= 21;
+        c = (b = VERSION.SDK_INT >= 23)?new f():null;
+    }
+}
