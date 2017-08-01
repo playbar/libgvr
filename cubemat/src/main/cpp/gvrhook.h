43e9b92ec0475850bb34b043233f5a966275327f
playbar <hgl868@126.com>
2017-08-01 09:12:12 +0800

    添加cubemat 工程

---
+++ cubemat/src/main/cpp/gvrhook.h
@@ -0,0 +1,13 @@
+
+#include <jni.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+JNIEXPORT void JNICALL Java_com_unity3d_unitygvr_GoogleVR_HookInit(JNIEnv * env , jobject obj ) ;
+
+#ifdef __cplusplus
+}
+#endif
+
