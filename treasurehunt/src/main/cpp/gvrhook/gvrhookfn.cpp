2edea9be798faf9f3fcfdb7df1087cdb42338763
playbar <hgl868@126.com>
2017-08-30 11:46:25 +0800

    添加hook 函数

--- treasurehunt/src/main/cpp/gvrhook/gvrhookfn.cpp
+++ treasurehunt/src/main/cpp/gvrhook/gvrhookfn.cpp
@@ -102,12 +102,100 @@ int32_t mj_gvr_clear_error(gvr_context* gvr)
 	return old_gvr_clear_error(gvr);
 }
 
+#define fn_gvr_pause_tracking "gvr_pause_tracking"
+void (*old_gvr_pause_tracking)(gvr_context* gvr) = NULL;
+void mj_gvr_pause_tracking(gvr_context* gvr)
+{
+    LOGI("mj_gvr_pause_tracking");
+    return old_gvr_pause_tracking(gvr);
+}
+
 #define fn_gvr_resume_tracking "gvr_resume_tracking"
 void (*old_gvr_resume_tracking)(gvr_context* gvr) = NULL;
 void mj_gvr_resume_tracking(gvr_context* gvr)
 {
     LOGI("mj_gvr_resume_tracking");
     return old_gvr_resume_tracking(gvr);
+}
+
+#define fn_gvr_reset_tracking "gvr_reset_tracking"
+void (*old_gvr_reset_tracking)(gvr_context* gvr) = NULL;
+void mj_gvr_reset_tracking(gvr_context* gvr)
+{
+	LOGI("mj_gvr_reset_tracking");
+	return old_gvr_reset_tracking(gvr);
+}
+
+#define fn_gvr_recenter_tracking "gvr_recenter_tracking"
+void (*old_gvr_recenter_tracking)(gvr_context* gvr) = NULL;
+void mj_gvr_recenter_tracking(gvr_context* gvr)
+{
+	LOGI("mj_gvr_recenter_tracking");
+	return old_gvr_recenter_tracking(gvr);
+}
+
+#define fn_gvr_set_default_viewer_profile "gvr_set_default_viewer_profile"
+bool (*old_gvr_set_default_viewer_profile)(gvr_context* gvr, const char* viewer_profile_uri) = NULL;
+bool mj_gvr_set_default_viewer_profile(gvr_context* gvr, const char* viewer_profile_uri)
+{
+    LOGI("mj_gvr_set_default_viewer_profile");
+    return old_gvr_set_default_viewer_profile(gvr, viewer_profile_uri);
+}
+
+#define fn_gvr_refresh_viewer_profile "gvr_refresh_viewer_profile"
+void (*old_gvr_refresh_viewer_profile)(gvr_context* gvr) = NULL;
+void mj_gvr_refresh_viewer_profile(gvr_context* gvr)
+{
+    LOGI("mj_gvr_refresh_viewer_profile");
+    return old_gvr_refresh_viewer_profile(gvr);
+}
+
+#define fn_gvr_get_viewer_vendor "gvr_get_viewer_vendor"
+const char* (*old_gvr_get_viewer_vendor)(const gvr_context* gvr) = NULL;
+const char* mj_gvr_get_viewer_vendor(const gvr_context* gvr)
+{
+    LOGI("gvr_get_viewer_vendor");
+    return old_gvr_get_viewer_vendor(gvr);
+}
+
+#define fn_gvr_get_viewer_model "gvr_get_viewer_model"
+const char* (*old_gvr_get_viewer_model)(const gvr_context* gvr) = NULL;
+const char* mj_gvr_get_viewer_model(const gvr_context* gvr)
+{
+    LOGI("mj_gvr_get_viewer_model");
+    return old_gvr_get_viewer_model(gvr);
+}
+
+#define fn_gvr_get_viewer_type "gvr_get_viewer_type"
+int32_t (*old_gvr_get_viewer_type)(const gvr_context* gvr) = NULL;
+int32_t mj_gvr_get_viewer_type(const gvr_context* gvr)
+{
+    LOGI("mj_gvr_get_viewer_type");
+    return old_gvr_get_viewer_type(gvr);
+}
+
+#define fn_gvr_get_eye_from_head_matrix "gvr_get_eye_from_head_matrix"
+gvr_mat4f (*old_gvr_get_eye_from_head_matrix)(const gvr_context* gvr, const int32_t eye)= NULL;
+gvr_mat4f mj_gvr_get_eye_from_head_matrix(const gvr_context* gvr, const int32_t eye)
+{
+    LOGI("mj_gvr_get_eye_from_head_matrix");
+    return old_gvr_get_eye_from_head_matrix(gvr, eye);
+}
+
+#define fn_gvr_get_window_bounds "gvr_get_window_bounds"
+gvr_recti (*old_gvr_get_window_bounds)(const gvr_context* gvr) = NULL;
+gvr_recti mj_gvr_get_window_bounds(const gvr_context* gvr)
+{
+    LOGI("gvr_get_window_bounds");
+    return old_gvr_get_window_bounds(gvr);
+}
+
+#define fn_gvr_compute_distorted_point "gvr_compute_distorted_point"
+void (*old_gvr_compute_distorted_point)(const gvr_context* gvr, const int32_t eye, const gvr_vec2f uv_in, gvr_vec2f uv_out[3]) = NULL;
+void mj_gvr_compute_distorted_point(const gvr_context* gvr, const int32_t eye, const gvr_vec2f uv_in, gvr_vec2f uv_out[3])
+{
+    LOGI("mj_gvr_compute_distorted_point");
+    return old_gvr_compute_distorted_point(gvr, eye, uv_in, uv_out);
 }
 
 #define fn_gvr_get_user_prefs "gvr_get_user_prefs"
@@ -135,6 +223,112 @@ void mj_gvr_initialize_gl(gvr_context* gvr)
 	return old_gvr_initialize_gl(gvr);
 }
 
+#define fn_gvr_get_async_reprojection_enabled "gvr_get_async_reprojection_enabled"
+bool (*old_gvr_get_async_reprojection_enabled)(const gvr_context* gvr) = NULL;
+bool mj_gvr_get_async_reprojection_enabled(const gvr_context* gvr)
+{
+    LOGI("mj_gvr_get_async_reprojection_enabled");
+    return old_gvr_get_async_reprojection_enabled(gvr);
+}
+
+#define fn_gvr_get_recommended_buffer_viewports "gvr_get_recommended_buffer_viewports"
+void (*old_gvr_get_recommended_buffer_viewports)(const gvr_context* gvr, gvr_buffer_viewport_list* viewport_list) = NULL;
+void mj_gvr_get_recommended_buffer_viewports(const gvr_context* gvr, gvr_buffer_viewport_list* viewport_list)
+{
+    LOGI("mj_gvr_get_recommended_buffer_viewports");
+    return old_gvr_get_recommended_buffer_viewports(gvr, viewport_list);
+}
+
+#define fn_gvr_get_screen_buffer_viewports "gvr_get_screen_buffer_viewports"
+void (*old_gvr_get_screen_buffer_viewports)(const gvr_context* gvr, gvr_buffer_viewport_list* viewport_list) = NULL;
+void mj_gvr_get_screen_buffer_viewports(const gvr_context* gvr, gvr_buffer_viewport_list* viewport_list)
+{
+	LOGI("mj_gvr_get_screen_buffer_viewports");
+	return old_gvr_get_screen_buffer_viewports(gvr, viewport_list);
+}
+
+#define fn_gvr_get_maximum_effective_render_target_size "gvr_get_maximum_effective_render_target_size"
+gvr_sizei (*old_gvr_get_maximum_effective_render_target_size)(const gvr_context* gvr) = NULL;
+gvr_sizei mj_gvr_get_maximum_effective_render_target_size(const gvr_context* gvr)
+{
+    LOGI("mj_gvr_get_maximum_effective_render_target_size");
+    return old_gvr_get_maximum_effective_render_target_size(gvr);
+}
+
+#define fn_gvr_get_screen_target_size "gvr_get_screen_target_size"
+gvr_sizei (*old_gvr_get_screen_target_size)(const gvr_context* gvr) = NULL;
+gvr_sizei mj_gvr_get_screen_target_size(const gvr_context* gvr)
+{
+    LOGI("mj_gvr_get_screen_target_size");
+	return old_gvr_get_screen_target_size(gvr);
+}
+
+#define fn_gvr_distort_to_screen "gvr_distort_to_screen"
+void (*old_gvr_distort_to_screen)(gvr_context* gvr, int32_t texture_id, const gvr_buffer_viewport_list* viewport_list,
+                           gvr_mat4f head_space_from_start_space, gvr_clock_time_point target_presentation_time) = NULL;
+void mj_gvr_distort_to_screen(gvr_context* gvr, int32_t texture_id, const gvr_buffer_viewport_list* viewport_list,
+                           gvr_mat4f head_space_from_start_space, gvr_clock_time_point target_presentation_time)
+{
+    LOGI("mj_gvr_distort_to_screen");
+    return old_gvr_distort_to_screen(gvr, texture_id, viewport_list, head_space_from_start_space, target_presentation_time);
+}
+
+#define fn_gvr_is_feature_supported "gvr_is_feature_supported"
+bool (*old_gvr_is_feature_supported)(const gvr_context* gvr, int32_t feature) = NULL;
+bool mj_gvr_is_feature_supported(const gvr_context* gvr, int32_t feature)
+{
+    LOGI("mj_gvr_is_feature_supported");
+    return old_gvr_is_feature_supported(gvr, feature);
+}
+
+#define fn_gvr_buffer_viewport_create "gvr_buffer_viewport_create"
+gvr_buffer_viewport* (*old_gvr_buffer_viewport_create)(gvr_context* gvr) = NULL;
+gvr_buffer_viewport* mj_gvr_buffer_viewport_create(gvr_context* gvr)
+{
+    LOGI("mj_gvr_buffer_viewport_create");
+    return old_gvr_buffer_viewport_create(gvr);
+}
+
+#define fn_gvr_buffer_viewport_list_create "gvr_buffer_viewport_list_create"
+gvr_buffer_viewport_list* (*old_gvr_buffer_viewport_list_create)(const gvr_context* gvr) = NULL;
+gvr_buffer_viewport_list* mj_gvr_buffer_viewport_list_create(const gvr_context* gvr)
+{
+    LOGI("mj_gvr_buffer_viewport_list_create");
+    return old_gvr_buffer_viewport_list_create(gvr);
+}
+
+#define fn_gvr_buffer_spec_create "gvr_buffer_spec_create"
+gvr_buffer_spec* (*old_gvr_buffer_spec_create)(gvr_context* gvr) = NULL;
+gvr_buffer_spec* mj_gvr_buffer_spec_create(gvr_context* gvr)
+{
+    LOGI("mj_gvr_buffer_spec_create");
+    return old_gvr_buffer_spec_create(gvr);
+}
+
+#define fn_gvr_swap_chain_create "gvr_swap_chain_create"
+gvr_swap_chain* (*old_gvr_swap_chain_create)(gvr_context* gvr, const gvr_buffer_spec** buffers, int32_t count) = NULL;
+gvr_swap_chain* mj_gvr_swap_chain_create(gvr_context* gvr, const gvr_buffer_spec** buffers, int32_t count)
+{
+    LOGI("mj_gvr_swap_chain_create");
+    return old_gvr_swap_chain_create(gvr, buffers, count);
+}
+
+#define fn_gvr_bind_default_framebuffer "gvr_bind_default_framebuffer"
+void (*old_gvr_bind_default_framebuffer)(gvr_context* gvr) = NULL;
+void mj_gvr_bind_default_framebuffer(gvr_context* gvr)
+{
+    LOGI("mj_gvr_bind_default_framebuffer");
+    return old_gvr_bind_default_framebuffer(gvr);
+}
+
+#define fn_gvr_apply_neck_model "gvr_apply_neck_model"
+gvr_mat4f (*old_gvr_apply_neck_model)(const gvr_context* gvr, gvr_mat4f head_space_from_start_space_rotation, float factor) = NULL;
+gvr_mat4f mj_gvr_apply_neck_model(const gvr_context* gvr, gvr_mat4f head_space_from_start_space_rotation, float factor)
+{
+	LOGI("mj_gvr_apply_neck_model");
+	return old_gvr_apply_neck_model(gvr, head_space_from_start_space_rotation, factor);
+}
+
 #define fn_gvr_set_display_metrics "gvr_set_display_metrics"
 int (*old_gvr_set_display_metrics)( int *a1, int a2, int a3, int a4, int a5, int a6) = NULL;
 int mj_gvr_set_display_metrics( int *a1, int widht, int height, int a4, int a5, int a6)
@@ -144,8 +338,8 @@ int mj_gvr_set_display_metrics( int *a1, int widht, int height, int a4, int a5, 
 }
 
 #define fn_gvr_set_display_synchronizer "gvr_set_display_synchronizer"
-int (*old_gvr_set_display_synchronizer)( int *a1, int a2) = NULL;
-int mj_gvr_set_display_synchronizer( int *a1, int a2)
+int (*old_gvr_set_display_synchronizer)( gvr_context *a1, int a2) = NULL;
+int mj_gvr_set_display_synchronizer( gvr_context *a1, int a2)
 {
     LOGI("mj_gvr_set_display_synchronizer");
     return old_gvr_set_display_synchronizer(a1, a2);
@@ -280,7 +474,31 @@ bool InitHook()
 			   &&HookToFunction(g_hGVR, fn_gvr_get_user_prefs, (void*)mj_gvr_get_user_prefs, (void**)&old_gvr_get_user_prefs)
 			   &&HookToFunction(g_hGVR, fn_gvr_destroy, (void*)mj_gvr_destroy, (void**)&old_gvr_destroy)
 			   &&HookToFunction(g_hGVR, fn_gvr_initialize_gl,(void*)mj_gvr_initialize_gl, (void**)&old_gvr_initialize_gl)
+               &&HookToFunction(g_hGVR, fn_gvr_get_async_reprojection_enabled,(void*)mj_gvr_get_async_reprojection_enabled, (void**)&old_gvr_get_async_reprojection_enabled)
+			   &&HookToFunction(g_hGVR, fn_gvr_get_recommended_buffer_viewports, (void*)mj_gvr_get_recommended_buffer_viewports, (void**)&old_gvr_get_recommended_buffer_viewports)
+			   &&HookToFunction(g_hGVR, fn_gvr_get_screen_buffer_viewports, (void*)mj_gvr_get_screen_buffer_viewports, (void**)&old_gvr_get_screen_buffer_viewports)
+			   &&HookToFunction(g_hGVR, fn_gvr_get_maximum_effective_render_target_size, (void*)mj_gvr_get_maximum_effective_render_target_size, (void**)&old_gvr_get_maximum_effective_render_target_size)
+			   &&HookToFunction(g_hGVR, fn_gvr_get_screen_target_size, (void*)mj_gvr_get_screen_target_size, (void**)&old_gvr_get_screen_target_size)
+			   &&HookToFunction(g_hGVR, fn_gvr_distort_to_screen, (void*)mj_gvr_distort_to_screen, (void**)&old_gvr_distort_to_screen)
+               &&HookToFunction(g_hGVR, fn_gvr_is_feature_supported, (void*)mj_gvr_is_feature_supported, (void**)&old_gvr_is_feature_supported)
+			   &&HookToFunction(g_hGVR, fn_gvr_buffer_viewport_create, (void*)mj_gvr_buffer_viewport_create, (void**)&old_gvr_buffer_viewport_create)
+               &&HookToFunction(g_hGVR, fn_gvr_buffer_viewport_list_create, (void*)mj_gvr_buffer_viewport_list_create, (void**)&old_gvr_buffer_viewport_list_create)
+               &&HookToFunction(g_hGVR, fn_gvr_buffer_spec_create, (void*)mj_gvr_buffer_spec_create, (void**)&old_gvr_buffer_spec_create)
+			   &&HookToFunction(g_hGVR, fn_gvr_swap_chain_create, (void*)mj_gvr_swap_chain_create, (void**)&old_gvr_swap_chain_create)
+			   &&HookToFunction(g_hGVR, fn_gvr_bind_default_framebuffer, (void*)mj_gvr_bind_default_framebuffer, (void**)&old_gvr_bind_default_framebuffer)
+			   &&HookToFunction(g_hGVR, fn_gvr_apply_neck_model, (void*)mj_gvr_apply_neck_model, (void**)&old_gvr_apply_neck_model)
+			   &&HookToFunction(g_hGVR, fn_gvr_pause_tracking, (void*)mj_gvr_pause_tracking, (void**)&old_gvr_pause_tracking)
 			   &&HookToFunction(g_hGVR, fn_gvr_resume_tracking, (void*)mj_gvr_resume_tracking, (void**)&old_gvr_resume_tracking)
+			   &&HookToFunction(g_hGVR, fn_gvr_reset_tracking, (void*)mj_gvr_reset_tracking, (void**)&old_gvr_reset_tracking)
+			   &&HookToFunction(g_hGVR, fn_gvr_recenter_tracking, (void*)mj_gvr_recenter_tracking, (void**)&old_gvr_recenter_tracking)
+			   &&HookToFunction(g_hGVR, fn_gvr_set_default_viewer_profile, (void*)mj_gvr_set_default_viewer_profile, (void**)&old_gvr_set_default_viewer_profile)
+               &&HookToFunction(g_hGVR, fn_gvr_refresh_viewer_profile, (void*)mj_gvr_refresh_viewer_profile, (void**)&old_gvr_refresh_viewer_profile)
+			   &&HookToFunction(g_hGVR, fn_gvr_get_viewer_vendor, (void*)mj_gvr_get_viewer_vendor, (void**)&old_gvr_get_viewer_vendor)
+			   &&HookToFunction(g_hGVR, fn_gvr_get_viewer_model, (void*)mj_gvr_get_viewer_model, (void**)&old_gvr_get_viewer_model)
+			   &&HookToFunction(g_hGVR, fn_gvr_get_viewer_type, (void*)mj_gvr_get_viewer_type, (void**)&old_gvr_get_viewer_type)
+               &&HookToFunction(g_hGVR, fn_gvr_get_eye_from_head_matrix, (void*)mj_gvr_get_eye_from_head_matrix, (void**)&old_gvr_get_eye_from_head_matrix)
+               &&HookToFunction(g_hGVR, fn_gvr_get_window_bounds, (void*)mj_gvr_get_window_bounds, (void**)&old_gvr_get_window_bounds)
+			   &&HookToFunction(g_hGVR, fn_gvr_compute_distorted_point, (void*)mj_gvr_compute_distorted_point, (void**)&old_gvr_compute_distorted_point)
 			   &&HookToFunction(g_hGVR, fn_gvr_set_display_synchronizer, (void*)mj_gvr_set_display_synchronizer, (void**)&old_gvr_set_display_synchronizer)
 			   &&HookToFunction(g_hGVR, fn_gvr_set_display_metrics, (void*)mj_gvr_set_display_metrics, (void**)&old_gvr_set_display_metrics)
 			   &&HookToFunction(g_hGVR, fn_Java_com_google_vr_cardboard_DisplaySynchronizer_nativeCreate, (void*)mj_Java_com_google_vr_cardboard_DisplaySynchronizer_nativeCreate, (void**)&old_Java_com_google_vr_cardboard_DisplaySynchronizer_nativeCreate)
@@ -289,7 +507,6 @@ bool InitHook()
 		if (bRet)
 		{
 			bRet = true;
-
 		}
 		else
 		{
