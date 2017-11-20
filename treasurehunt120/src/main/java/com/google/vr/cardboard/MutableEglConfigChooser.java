648e6f44e6f2c5b4069065e4059d9069f0983ce3
playbar <hgl868@126.com>
2017-11-20 14:42:14 +0800

    修改gvr 破解方法文档

--- treasurehunt120/src/main/java/com/google/vr/cardboard/MutableEglConfigChooser.java
+++ treasurehunt120/src/main/java/com/google/vr/cardboard/MutableEglConfigChooser.java
@@ -23,12 +23,25 @@ public class MutableEglConfigChooser implements EGLConfigChooser {
     }
 
     public EGLConfig chooseConfig(EGL10 var1, EGLDisplay var2) {
-        int[] var3 = new int[]{12324, 8, 12323, 8, 12322, 8, 12321, 0, 12325, 0, 12326, 0, 12352, 64, 12339, 4, 12344};
+//        int[] var3 = new int[]{12324, 8, 12323, 8, 12322, 8, 12321, 0, 12325, 0, 12326, 0, 12352, 64, 12339, 4, 12344};
+        int[] var3 = new int[]{
+                EGL10.EGL_RED_SIZE, 8,
+                EGL10.EGL_GREEN_SIZE, 8,
+                EGL10.EGL_BLUE_SIZE, 8,
+//                EGL10.EGL_ALPHA_SIZE, 0,
+//                EGL10.EGL_DEPTH_SIZE, 0,
+//                EGL10.EGL_STENCIL_SIZE, 0,
+//                EGL10.EGL_RENDERABLE_TYPE, 64,
+//                EGL10.EGL_SURFACE_TYPE, EGL10.EGL_WINDOW_BIT,
+//                EGLExt.EGL_MUTABLE_RENDER_BUFFER_BIT_KHR,
+//                EGL10.EGL_SURFACE_TYPE, 0x1004,
+                EGL10.EGL_NONE
+        };
         int[] var4 = new int[1];
         if(!var1.eglChooseConfig(var2, var3, (EGLConfig[])null, 0, var4) && this.forceMutableBuffer) {
             throw new IllegalArgumentException("eglChooseConfig failed");
         } else {
-            var3[15] = 4;
+//            var3[15] = 4;
             if(!var1.eglChooseConfig(var2, var3, (EGLConfig[])null, 0, var4)) {
                 throw new IllegalArgumentException("eglChooseConfig failed");
             } else {
@@ -56,17 +69,24 @@ public class MutableEglConfigChooser implements EGLConfigChooser {
         EGLConfig[] var4 = var2;
         int var5 = var2.length;
 
-        for(int var6 = 0; var6 < var5; ++var6) {
+//        for(int var6 = 0; var6 < var5; ++var6) {
+//            EGLConfig var7 = var4[var6];
+//            int var8 = findConfigAttrib(var0, var1, var7, 12325, 0);
+//            int var9 = findConfigAttrib(var0, var1, var7, 12326, 0);
+//            int var10 = findConfigAttrib(var0, var1, var7, 12324, 0);
+//            int var11 = findConfigAttrib(var0, var1, var7, 12323, 0);
+//            int var12 = findConfigAttrib(var0, var1, var7, 12322, 0);
+//            int var13 = findConfigAttrib(var0, var1, var7, 12339, 0);
+//            if(var10 == 8 && var11 == 8 && var12 == 8 && var8 == 0 && var9 == 0 && (!var3 || (var13 & 4) != 0)) {
+//                return var7;
+//            }
+//        }
+
+        for(int var6 = 0; var6 < var5; ++var6)
+        {
             EGLConfig var7 = var4[var6];
-            int var8 = findConfigAttrib(var0, var1, var7, 12325, 0);
-            int var9 = findConfigAttrib(var0, var1, var7, 12326, 0);
-            int var10 = findConfigAttrib(var0, var1, var7, 12324, 0);
-            int var11 = findConfigAttrib(var0, var1, var7, 12323, 0);
-            int var12 = findConfigAttrib(var0, var1, var7, 12322, 0);
-            int var13 = findConfigAttrib(var0, var1, var7, 12339, 0);
-            if(var10 == 8 && var11 == 8 && var12 == 8 && var8 == 0 && var9 == 0 && (!var3 || (var13 & 4) != 0)) {
-                return var7;
-            }
+            return var7;
+
         }
 
         return null;
